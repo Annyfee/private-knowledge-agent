8a5ecf22800311985ab2ccdb25c39ed0e8d2c10c
Annyfee <2287551746@qq.com>
2026-03-19 09:38:36 +0800

    refactor(frontend):更新前端代码，改为本地知识库搜寻相关

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,3 @@
-# 初始化配置
 import uuid
 import streamlit as st
 from chat_flow import handle_chat_turn
@@ -7,10 +6,8 @@ from ui import setup_page, render_sidebar, render_header, render_history
 
 setup_page()
 
-# 状态初始化
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
-# 用message列表用来存结果
 if "message" not in st.session_state:
     st.session_state.message = []
 
@@ -25,8 +22,6 @@ if new_chat_clicked:
 render_header()
 render_history()
 
-
-# 处理用户输入(核心)
-prompt = st.chat_input("请输入你的研究课题...")
+prompt = st.chat_input("请输入您想从本地资料中洞察什么内容...")
 if prompt:
     handle_chat_turn(prompt)
